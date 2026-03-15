241e197851e6b6b9a7752eed3bcb6af91826bc0c
lamhn9999 <hongoclamm@gmail.com>
2026-03-16 04:34:36 +0700

    fix: switch control key to caps lock, grab keyboard at startup
    - Replace shift with caps lock (code 58) as the mouse control modifier
    - Grab keyboard at startup so the OS never sees raw key events,
      fixing bugs that caused capitalized characters

--- Nami.c
+++ Nami.c
@@ -7,6 +7,14 @@
 #include <sys/ioctl.h>
 #include <sys/time.h>
 
+int shift = 0;
+int shiftVal = 58;
+
+int speedW = -10;
+int speedS = 10;
+int speedA = -10;
+int speedD = 10;
+
 void emit(int fd, int type, int code, int val)
 {
     struct input_event ie;
@@ -24,15 +32,21 @@ void forward(int ufd, struct input_event *ev)
     emit(ufd, EV_SYN, SYN_REPORT, 0);
 }
 
+void resetSpeed()
+{
+    speedW = -10;
+    speedS = 10;
+    speedA = -10;
+    speedD = 10;
+}
+
 int main()
 {
     int ufd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-
     ioctl(ufd, UI_SET_EVBIT, EV_KEY);
     ioctl(ufd, UI_SET_KEYBIT, BTN_LEFT);
     for(int i = 0; i < KEY_MAX; i++)
         ioctl(ufd, UI_SET_KEYBIT, i);
-
     ioctl(ufd, UI_SET_EVBIT, EV_REL);
     ioctl(ufd, UI_SET_RELBIT, REL_X);
     ioctl(ufd, UI_SET_RELBIT, REL_Y);
@@ -46,97 +60,45 @@ int main()
     sleep(1);
 
     int kfd = open("/dev/input/event3", O_RDONLY);
+    ioctl(kfd, EVIOCGRAB, 1);
+
     struct input_event ev;
-
-    int shift = 0;
-    int ctrl  = 0;
-    int alt   = 0;
-
     while(1)
     {
         read(kfd, &ev, sizeof(ev));
         if(ev.type != EV_KEY)
             continue;
 
-        /* --- Track Ctrl --- */
-        if(ev.code == KEY_LEFTCTRL || ev.code == KEY_RIGHTCTRL)
+        /* --- Window button handling --- */
+        if(ev.code == shiftVal)
         {
-            ctrl = (ev.value != 0);
-            if(ctrl && shift)
-            {
-                shift = 0;
-                emit(ufd, EV_KEY, KEY_LEFTSHIFT, 0);
-                emit(ufd, EV_SYN, SYN_REPORT, 0);
-                ioctl(kfd, EVIOCGRAB, 0);
-            }
-            forward(ufd, &ev);
+            shift = (ev.value > 0);
+            if(!shift) resetSpeed();
             continue;
         }
-
-        /* --- Track Alt --- */
-        if(ev.code == KEY_LEFTALT || ev.code == KEY_RIGHTALT)
-        {
-            alt = (ev.value != 0);
-            if(alt && shift)
-            {
-                shift = 0;
-                emit(ufd, EV_KEY, KEY_LEFTSHIFT, 0);
-                emit(ufd, EV_SYN, SYN_REPORT, 0);
-                ioctl(kfd, EVIOCGRAB, 0);
-            }
-            forward(ufd, &ev);
-            continue;
-        }
-
-        /* --- Shift handling --- */
-        if(ev.code == KEY_LEFTSHIFT || ev.code == KEY_RIGHTSHIFT)
-        {
-            if(ev.value == 1 && !shift && !ctrl && !alt)
-            {
-                shift = 1;
-                ioctl(kfd, EVIOCGRAB, 1);
-            }
-            else if(ev.value == 0 && shift)
-            {
-                shift = 0;
-                emit(ufd, EV_KEY, ev.code, 0);
-                emit(ufd, EV_SYN, SYN_REPORT, 0);
-                ioctl(kfd, EVIOCGRAB, 0);
-            }
-            else
-            {
-                forward(ufd, &ev);
-            }
-            continue;
-        }
-
-        if(ctrl || alt)
-        {
-            forward(ufd, &ev);
-            continue;
-        }
-
-        if(ev.value != 1)
-            continue;
 
         if(shift)
         {
-            switch(ev.code)
+            if(ev.value == 0)
+            {
+                resetSpeed();
+            }
+            else switch(ev.code)
             {
                 case KEY_W:
-                    emit(ufd, EV_REL, REL_Y, -10);
+                    emit(ufd, EV_REL, REL_Y, speedW--);
                     emit(ufd, EV_SYN, SYN_REPORT, 0);
                     break;
                 case KEY_S:
-                    emit(ufd, EV_REL, REL_Y, 10);
+                    emit(ufd, EV_REL, REL_Y, speedS++);
                     emit(ufd, EV_SYN, SYN_REPORT, 0);
                     break;
                 case KEY_A:
-                    emit(ufd, EV_REL, REL_X, -10);
+                    emit(ufd, EV_REL, REL_X, speedA--);
                     emit(ufd, EV_SYN, SYN_REPORT, 0);
                     break;
                 case KEY_D:
-                    emit(ufd, EV_REL, REL_X, 10);
+                    emit(ufd, EV_REL, REL_X, speedD++);
                     emit(ufd, EV_SYN, SYN_REPORT, 0);
                     break;
                 case KEY_J:
@@ -146,13 +108,12 @@ int main()
                     emit(ufd, EV_SYN, SYN_REPORT, 0);
                     break;
                 default:
-                    forward(ufd, &ev);
                     break;
             }
         }
         else
         {
-            printf("letter: %d\n", ev.code);
+            forward(ufd, &ev);
         }
     }
 }
