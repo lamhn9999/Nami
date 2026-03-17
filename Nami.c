f51fe684dc2462d235537bf35009c32db01c9300
lamhn9999 <hongoclamm@gmail.com>
2026-03-18 00:33:14 +0700

    refactor: Nami.c refactored
    Refactored the core logic to use a cleaner approach, replacing hardcoded switch-case blocks with a structured mapping system.

--- Nami.c
+++ Nami.c
@@ -8,17 +8,39 @@
 #include <sys/time.h>
 #include <stdbool.h>
 
-int shift = 0;
-int shiftVal = 58;
+/** Configuration **/
+#define SHIFT_KEY KEY_CAPSLOCK
+#define CLICK_KEY KEY_J
+#define BASE_SPEED 10
+#define POLL_DELAY 8000 // 8ms for smooth movement
 
-int speedW = -10, speedX = 10, speedA = -10, speedD = 10;
-char holding1 = 0, holding2 = 0;
-bool isPressing = false;
+typedef struct
+{
+    int code;
+    char label;
+    int *speed;
+    int axis; // REL_X or REL_Y
+} KeyMap;
 
+/** Global State **/
+int speedW = -BASE_SPEED, speedX = BASE_SPEED, speedA = -BASE_SPEED, speedD = BASE_SPEED;
+char holding[2] = {0, 0};
+bool is_clicking = false;
+
+// Map keys to their behavior
+KeyMap movement_keys[] =
+{
+    {KEY_W, 'W', &speedW, REL_Y},
+    {KEY_X, 'X', &speedX, REL_Y},
+    {KEY_A, 'A', &speedA, REL_X},
+    {KEY_D, 'D', &speedD, REL_X}
+};
+const int KEY_COUNT = 4;
+
+/** Core Logic **/
 void emit(int fd, int type, int code, int val)
 {
-    struct input_event ie;
-    memset(&ie, 0, sizeof(ie));
+    struct input_event ie = {0};
     gettimeofday(&ie.time, NULL);
     ie.type = type;
     ie.code = code;
@@ -26,195 +48,143 @@ void emit(int fd, int type, int code, int val)
     write(fd, &ie, sizeof(ie));
 }
 
-void forward(int ufd, struct input_event *ev)
+void sync_report(int fd)
 {
-    emit(ufd, ev->type, ev->code, ev->value);
-    emit(ufd, EV_SYN, SYN_REPORT, 0);
+    emit(fd, EV_SYN, SYN_REPORT, 0);
 }
 
-void report(int ufd, char direction)
+void reset_state()
 {
-    if (direction == 'W') emit(ufd, EV_REL, REL_Y, speedW);
-    else if (direction == 'X') emit(ufd, EV_REL, REL_Y, speedX);
-    else if (direction == 'A') emit(ufd, EV_REL, REL_X, speedA);
-    else if (direction == 'D') emit(ufd, EV_REL, REL_X, speedD);
-    emit(ufd, EV_SYN, SYN_REPORT, 0);
+    speedW = -BASE_SPEED;
+    speedX = BASE_SPEED;
+    speedA = -BASE_SPEED;
+    speedD = BASE_SPEED;
+    holding[0] = holding[1] = 0;
 }
 
-void incrSpeed(char direction)
+void update_holding(char label, bool pressed)
 {
-    switch(direction)
+    if (pressed)
     {
-    case 'W':
-        speedW--;
-        break;
-    case 'X':
-        speedX++;
-        break;
-    case 'A':
-        speedA--;
-        break;
-    case 'D':
-        speedD++;
-        break;
+        if (holding[0] != label)
+        {
+            holding[1] = holding[0];
+            holding[0] = label;
+        }
+    }
+    else
+    {
+        if (holding[0] == label) holding[0] = holding[1];
+        holding[1] = 0;
     }
 }
 
-void resetSpeed()
+void perform_movement(int ufd)
 {
-    speedW = -10;
-    speedX = 10;
-    speedA = -10;
-    speedD = 10;
-}
+    for (int i = 0; i < 2; i++)
+    {
+        if (holding[i] == 0) continue;
 
-void setHolding(char direction)
-{
-    if (holding1 != direction)
-    {
-        holding2 = holding1;
-        holding1 = direction;
+        for (int j = 0; j < KEY_COUNT; j++)
+        {
+            if (movement_keys[j].label == holding[i])
+            {
+                emit(ufd, EV_REL, movement_keys[j].axis, *movement_keys[j].speed);
+                // Increment speed (absolute value)
+                if (*movement_keys[j].speed > 0) (*movement_keys[j].speed)++;
+                else (*movement_keys[j].speed)--;
+            }
+        }
     }
-}
-
-void resetHolding(char direction)
-{
-    if (holding1 == direction)
-    {
-        holding1 = holding2;
-        holding2 = 0;
-    }
-    else if (holding2 == direction)
-    {
-        holding2 = 0;
-    }
+    sync_report(ufd);
 }
 
 int main()
 {
     int ufd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    int kfd = open("/dev/input/event3", O_RDONLY); // Note: verify your event ID
+
+    if (ufd < 0 || kfd < 0) return perror("Open failed"), 1;
+
+    // Setup uinput device
     ioctl(ufd, UI_SET_EVBIT, EV_KEY);
     ioctl(ufd, UI_SET_KEYBIT, BTN_LEFT);
-
-    for(int i = 0; i < KEY_MAX; i++)
-        ioctl(ufd, UI_SET_KEYBIT, i);
-
     ioctl(ufd, UI_SET_EVBIT, EV_REL);
     ioctl(ufd, UI_SET_RELBIT, REL_X);
     ioctl(ufd, UI_SET_RELBIT, REL_Y);
+    for (int i = 0; i < KEY_MAX; i++) ioctl(ufd, UI_SET_KEYBIT, i);
 
-    struct uinput_setup usetup;
-    memset(&usetup, 0, sizeof(usetup));
+    struct uinput_setup usetup = {0};
     usetup.id.bustype = BUS_USB;
-    strcpy(usetup.name, "Nami-Mouse");
+    strcpy(usetup.name, "Nami-Pro-Mouse");
     ioctl(ufd, UI_DEV_SETUP, &usetup);
     ioctl(ufd, UI_DEV_CREATE);
-
-    int kfd = open("/dev/input/event3", O_RDONLY);
-    if (kfd < 0)
-    {
-        perror("Could not open kfd");
-        return 1;
-    }
 
     sleep(1);
     ioctl(kfd, EVIOCGRAB, 1);
 
     struct input_event ev;
-    while(read(kfd, &ev, sizeof(ev)) > 0)
-    {
-        if(ev.type != EV_KEY) continue;
+    bool shift_active = false;
 
-        if(ev.code == shiftVal)
+    while (read(kfd, &ev, sizeof(ev)) > 0)
+    {
+        if (ev.type != EV_KEY) continue;
+
+        // Toggle Shift Mode
+        if (ev.code == SHIFT_KEY)
         {
-            shift = (ev.value > 0);
-            if(!shift)
+            shift_active = (ev.value > 0);
+            if (!shift_active)
             {
-                resetSpeed();
-                holding1 = holding2 = 0;
-                if(isPressing)
-                {
-                    emit(ufd, EV_KEY, BTN_LEFT, 0);
-                    emit(ufd, EV_SYN, SYN_REPORT, 0);
-                    isPressing = false;
-                }
+                if (is_clicking) emit(ufd, EV_KEY, BTN_LEFT, 0);
+                reset_state();
+                sync_report(ufd);
             }
             continue;
         }
 
-        if(shift)
+        if (shift_active)
         {
-            if(ev.value == 0)
-            {
-                switch(ev.code)
-                {
-                case KEY_W:
-                    resetHolding('W');
-                    break;
-                case KEY_X:
-                    resetHolding('X');
-                    break;
-                case KEY_A:
-                    resetHolding('A');
-                    break;
-                case KEY_D:
-                    resetHolding('D');
-                    break;
-                case KEY_J:
-                    emit(ufd, EV_KEY, BTN_LEFT, 0);
-                    emit(ufd, EV_SYN, SYN_REPORT, 0);
-                    isPressing = false;
-                    break;
-                }
-                resetSpeed();
-            }
-            else
-            {
-                if (ev.value == 1)
-                {
-                    switch(ev.code)
-                    {
-                    case KEY_W:
-                        setHolding('W');
-                        break;
-                    case KEY_X:
-                        setHolding('X');
-                        break;
-                    case KEY_A:
-                        setHolding('A');
-                        break;
-                    case KEY_D:
-                        setHolding('D');
-                        break;
-                    case KEY_J:
-                        emit(ufd, EV_KEY, BTN_LEFT, 1);
-                        emit(ufd, EV_SYN, SYN_REPORT, 0);
-                        isPressing = true;
-                        break;
-                    }
-                }
+            bool is_pressed = (ev.value > 0);
+            bool handled = false;
 
-                if (holding1 != 0)
+            // Handle Movement Keys
+            for (int i = 0; i < KEY_COUNT; i++)
+            {
+                if (ev.code == movement_keys[i].code)
                 {
-                    report(ufd, holding1);
-                    incrSpeed(holding1);
+                    update_holding(movement_keys[i].label, is_pressed);
+                    if (!is_pressed) reset_state();
+                    handled = true;
+                    break;
                 }
-                if (holding2 != 0)
-                {
-                    report(ufd, holding2);
-                    incrSpeed(holding2);
-                }
+            }
+
+            // Handle Clicking
+            if (ev.code == CLICK_KEY)
+            {
+                is_clicking = is_pressed;
+                emit(ufd, EV_KEY, BTN_LEFT, is_pressed ? 1 : 0);
+                sync_report(ufd);
+                handled = true;
+            }
+
+            // If a key is held, execute movement and ramp speed
+            if (holding[0] != 0)
+            {
+                perform_movement(ufd);
+                usleep(POLL_DELAY); // Prevents CPU max-out and smooths ramp
             }
         }
         else
         {
-            forward(ufd, &ev);
+            // Passthrough mode
+            emit(ufd, ev.type, ev.code, ev.value);
+            sync_report(ufd);
         }
     }
 
     ioctl(kfd, EVIOCGRAB, 0);
     ioctl(ufd, UI_DEV_DESTROY);
-    close(ufd);
-    close(kfd);
     return 0;
 }
