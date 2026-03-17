9d744dab04f54a9f69f257a717fb260b9212ae7f
lamhn9999 <hongoclamm@gmail.com>
2026-03-18 00:24:47 +0700

    feat: implement diagonal mouse movement and persistent button state
    - Replaced S with X to bypass hardware ghosting/conflict issues.
    - Added support for persistent BTN_LEFT holding via the J key.

--- Nami.c
+++ Nami.c
@@ -6,14 +6,14 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/time.h>
+#include <stdbool.h>
 
 int shift = 0;
 int shiftVal = 58;
 
-int speedW = -10;
-int speedS = 10;
-int speedA = -10;
-int speedD = 10;
+int speedW = -10, speedX = 10, speedA = -10, speedD = 10;
+char holding1 = 0, holding2 = 0;
+bool isPressing = false;
 
 void emit(int fd, int type, int code, int val)
 {
@@ -32,12 +32,62 @@ void forward(int ufd, struct input_event *ev)
     emit(ufd, EV_SYN, SYN_REPORT, 0);
 }
 
+void report(int ufd, char direction)
+{
+    if (direction == 'W') emit(ufd, EV_REL, REL_Y, speedW);
+    else if (direction == 'X') emit(ufd, EV_REL, REL_Y, speedX);
+    else if (direction == 'A') emit(ufd, EV_REL, REL_X, speedA);
+    else if (direction == 'D') emit(ufd, EV_REL, REL_X, speedD);
+    emit(ufd, EV_SYN, SYN_REPORT, 0);
+}
+
+void incrSpeed(char direction)
+{
+    switch(direction)
+    {
+    case 'W':
+        speedW--;
+        break;
+    case 'X':
+        speedX++;
+        break;
+    case 'A':
+        speedA--;
+        break;
+    case 'D':
+        speedD++;
+        break;
+    }
+}
+
 void resetSpeed()
 {
     speedW = -10;
-    speedS = 10;
+    speedX = 10;
     speedA = -10;
     speedD = 10;
+}
+
+void setHolding(char direction)
+{
+    if (holding1 != direction)
+    {
+        holding2 = holding1;
+        holding1 = direction;
+    }
+}
+
+void resetHolding(char direction)
+{
+    if (holding1 == direction)
+    {
+        holding1 = holding2;
+        holding2 = 0;
+    }
+    else if (holding2 == direction)
+    {
+        holding2 = 0;
+    }
 }
 
 int main()
@@ -45,8 +95,10 @@ int main()
     int ufd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
     ioctl(ufd, UI_SET_EVBIT, EV_KEY);
     ioctl(ufd, UI_SET_KEYBIT, BTN_LEFT);
+
     for(int i = 0; i < KEY_MAX; i++)
         ioctl(ufd, UI_SET_KEYBIT, i);
+
     ioctl(ufd, UI_SET_EVBIT, EV_REL);
     ioctl(ufd, UI_SET_RELBIT, REL_X);
     ioctl(ufd, UI_SET_RELBIT, REL_Y);
@@ -54,26 +106,39 @@ int main()
     struct uinput_setup usetup;
     memset(&usetup, 0, sizeof(usetup));
     usetup.id.bustype = BUS_USB;
-    strcpy(usetup.name, "keyboard-mouse");
+    strcpy(usetup.name, "Nami-Mouse");
     ioctl(ufd, UI_DEV_SETUP, &usetup);
     ioctl(ufd, UI_DEV_CREATE);
-    sleep(1);
 
     int kfd = open("/dev/input/event3", O_RDONLY);
+    if (kfd < 0)
+    {
+        perror("Could not open kfd");
+        return 1;
+    }
+
+    sleep(1);
     ioctl(kfd, EVIOCGRAB, 1);
 
     struct input_event ev;
-    while(1)
+    while(read(kfd, &ev, sizeof(ev)) > 0)
     {
-        read(kfd, &ev, sizeof(ev));
-        if(ev.type != EV_KEY)
-            continue;
+        if(ev.type != EV_KEY) continue;
 
-        /* --- Window button handling --- */
         if(ev.code == shiftVal)
         {
             shift = (ev.value > 0);
-            if(!shift) resetSpeed();
+            if(!shift)
+            {
+                resetSpeed();
+                holding1 = holding2 = 0;
+                if(isPressing)
+                {
+                    emit(ufd, EV_KEY, BTN_LEFT, 0);
+                    emit(ufd, EV_SYN, SYN_REPORT, 0);
+                    isPressing = false;
+                }
+            }
             continue;
         }
 
@@ -81,34 +146,64 @@ int main()
         {
             if(ev.value == 0)
             {
-                resetSpeed();
-            }
-            else switch(ev.code)
-            {
+                switch(ev.code)
+                {
                 case KEY_W:
-                    emit(ufd, EV_REL, REL_Y, speedW--);
-                    emit(ufd, EV_SYN, SYN_REPORT, 0);
+                    resetHolding('W');
                     break;
-                case KEY_S:
-                    emit(ufd, EV_REL, REL_Y, speedS++);
-                    emit(ufd, EV_SYN, SYN_REPORT, 0);
+                case KEY_X:
+                    resetHolding('X');
                     break;
                 case KEY_A:
-                    emit(ufd, EV_REL, REL_X, speedA--);
-                    emit(ufd, EV_SYN, SYN_REPORT, 0);
+                    resetHolding('A');
                     break;
                 case KEY_D:
-                    emit(ufd, EV_REL, REL_X, speedD++);
-                    emit(ufd, EV_SYN, SYN_REPORT, 0);
+                    resetHolding('D');
                     break;
                 case KEY_J:
-                    emit(ufd, EV_KEY, BTN_LEFT, 1);
-                    emit(ufd, EV_SYN, SYN_REPORT, 0);
                     emit(ufd, EV_KEY, BTN_LEFT, 0);
                     emit(ufd, EV_SYN, SYN_REPORT, 0);
+                    isPressing = false;
                     break;
-                default:
-                    break;
+                }
+                resetSpeed();
+            }
+            else
+            {
+                if (ev.value == 1)
+                {
+                    switch(ev.code)
+                    {
+                    case KEY_W:
+                        setHolding('W');
+                        break;
+                    case KEY_X:
+                        setHolding('X');
+                        break;
+                    case KEY_A:
+                        setHolding('A');
+                        break;
+                    case KEY_D:
+                        setHolding('D');
+                        break;
+                    case KEY_J:
+                        emit(ufd, EV_KEY, BTN_LEFT, 1);
+                        emit(ufd, EV_SYN, SYN_REPORT, 0);
+                        isPressing = true;
+                        break;
+                    }
+                }
+
+                if (holding1 != 0)
+                {
+                    report(ufd, holding1);
+                    incrSpeed(holding1);
+                }
+                if (holding2 != 0)
+                {
+                    report(ufd, holding2);
+                    incrSpeed(holding2);
+                }
             }
         }
         else
@@ -116,4 +211,10 @@ int main()
             forward(ufd, &ev);
         }
     }
+
+    ioctl(kfd, EVIOCGRAB, 0);
+    ioctl(ufd, UI_DEV_DESTROY);
+    close(ufd);
+    close(kfd);
+    return 0;
 }
